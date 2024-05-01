2c2212b2acd9ab5a280ea03d6645410397a1ef11
meo209 <redslyice@outlook.de>
2024-05-01 14:22:27 +0200

    Initial commit

---
+++ src/main/kotlin/org/openartifact/debug/components/CameraController.kt
@@ -0,0 +1,38 @@
+package org.openartifact.debug.components
+
+import glm_.vec3.Vec3
+import org.openartifact.artifact.core.input.*
+import org.openartifact.artifact.game.Component
+import org.openartifact.artifact.game.nodes.CameraNode
+
+class CameraController : Component() {
+
+    override fun update(physicsDeltaTime: Double) {
+        val camera = parent as CameraNode
+
+        // Adjust speed based on whether the left shift key is pressed
+        val speed : Float = if (getKeyDown(KEY_LEFT_SHIFT)) 8f * physicsDeltaTime.toFloat() else 2f * physicsDeltaTime.toFloat()
+
+        val camInc = Vec3()
+
+        // Process key inputs to calculate movement increments
+        keyMap {
+            KEY_W to { camInc.z = -1f }
+            KEY_A to { camInc.x = -1f }
+            KEY_S to { camInc.z = 1f }
+            KEY_D to { camInc.x = 1f }
+
+            KEY_E to { camInc.y = 1f }
+            KEY_Q to { camInc.y = -1f }
+        }.process()
+
+        // Apply movement increments to the camera
+        camera.move(camInc.x, camInc.y, camInc.z, speed)
+
+        mouseInput(0.5f, getMouseButtonDown(MOUSE_BUTTON_2)) { x, y ->
+            camera.rotate(x, y, 0f)
+        }
+    }
+
+
+}
